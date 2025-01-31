fc92153ee93e143c6da2d282cec7004f8281b6ef
Shantanu Mane <shantanu.mane.200@outlook.com>
2025-01-31 13:01:14 +0530

    fix: modified build distibutable to use cjs

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -5,7 +5,7 @@ const {
 } = process.env;
 
 export const envConfig = {
-    LEETCODE_GRAPHQL_ENDPOINT: LEETCODE_GRAPHQL_ENDPOINT,
+    LEETCODE_GRAPHQL_ENDPOINT: "https://leetcode.com/graphql",
     DEFAULT_LANGUAGE: "Python3" ,
     hostname: hostname(),
 };
